859f99f5e8027a255202f630fb551ff56c9c262c
BunnyNoBugs <eugene200020@gmail.com>
2024-09-24 17:35:57 +0300

    add query planning

--- rag_pipeline.py
+++ rag_pipeline.py
@@ -7,9 +7,11 @@ from langchain_core.output_parsers.base import BaseOutputParser
 from langchain.output_parsers.retry import RetryOutputParser
 from langchain_core.runnables import chain, RunnableParallel, RunnableLambda
 from langchain_core.exceptions import OutputParserException
+from langchain.docstore.document import Document
 from operator import itemgetter
 from langchain_core.vectorstores import VectorStoreRetriever
 from typing import List
+import ast
 
 answer_prompt_template = """
 <|begin_of_text|>
@@ -37,6 +39,34 @@ hyde_prompt_template = """
 Абзац:
 """
 
+planner_prompt_template = """
+<|begin_of_text|>
+<|start_header_id|>system<|end_header_id|>
+Ты система осуществляющая планирование запросов нужной для пользователя информации в векторной базе с документами разделенными на небольшие сегменты. \
+Для каждого поступившего запроса от пользователя \
+твоя задача, при необходимости, разделить его на несколько запросов к векторной базе данных для получения нужной для ответа информации. \
+Ты должен написать итоговые запросы "Запросы:", а потом написать объяснение "Объяснение:". И запросы и объяснение должны быть на русском языке. \
+Например, вопрос "назови столицу России" не нуждается в разделении, необходимая информация о столице России будет извлечена. \
+В свою очередь, запрос "Сравни автомобили ауди q5 и audi q7" уже должен быть разделен на два: "Какие преимущества и недостатки и audi q5" и "Какие преимущества и недостатки у audi q7", т.к. нам \
+нужна информация по обоим объектам для проведения сравнения. 
+Пользуйся следующим набором правил:
+1. Если встречается указание на год или день, то учти, что сейчас 14 июня 2024 года и заменяй в итоговых запросах на конкретную информацию для улучшения поиска (например, "в этом году" должно быть изменено на "2024 году", "сегодня" нужно заменить на "14 июня 2024 года"). 
+2. Компания для которой ты работаешь и в которой работают пользователи называется Татнефть, где уместно заменяй на конкретное название.
+3. Каждый следующий вопрос не должен опираться на результат ответа предыдущего (нельзя: "Какие из этих технологий..."). Агрегация информации произойдет позднее.
+4. Компании конкуренты Татнефти в нефтяной области: Газпромнефть, Башнефть, Роснефть, Лукоил.
+5. Если будешь отвечать на английском получишь штраф.
+6. Учитывай что у пользовательского запроса есть контекст, который нельзя терять.
+7. Если пользовательский запрос можно разделить на несколько, не забудь добавить необходимый контекст для независимого поиска по базе.
+
+Результат выведи списком запросов после ключевого слова ЗАПРОСЫ: ["запрос1", "запрос2"...]
+<|eot_id|>
+<|start_header_id|>user<|end_header_id|>
+
+{context}
+
+<|eot_id|>
+<|start_header_id|>assistant<|end_header_id|>"""
+
 
 @chain
 def prepare_context(retrieval_result):
@@ -49,6 +79,29 @@ class HydeOutputParser(BaseOutputParser[list[str]]):
     def parse(self, text: str) -> str:
         """Parse by splitting."""
         result = text.split('\nАбзац:\n')[1]
+        return result
+
+
+class PlannerOutputParser(BaseOutputParser[List[str]]):
+    def parse(self, text: str) -> List[str]:
+        """Parse by splitting."""
+        try:
+            result = ast.literal_eval(text.split('\nЗАПРОСЫ:')[1].strip().split(']')[0] + ']')
+        except:
+            raise OutputParserException('Failed to parse')
+        return result
+
+
+class BatchQueryRetriever:
+    def __init__(self, retriever: VectorStoreRetriever):
+        self._retriever = retriever
+
+    def _remove_duplicates(self, docs: List[Document]) -> List[Document]:
+        return list({doc.page_content: doc for doc in docs}.values())
+
+    def do_batch(self, queries: List[str]) -> List[Document]:
+        result = self._retriever.batch(queries)
+        result = self._remove_duplicates([doc for docs in result for doc in docs])
         return result
 
 
@@ -79,8 +132,33 @@ class RAGPipeline:
                     hyde_chain
                     | RunnableParallel(retrieved_docs=retriever, hyde_doc=RunnablePassthrough())
             )
+
+        elif self._do_planning:
+            planner_prompt = PromptTemplate(input_variables=["context"], template=planner_prompt_template)
+            planner_parser = PlannerOutputParser()
+            retry_planner_parser = RetryOutputParser.from_llm(
+                parser=planner_parser,
+                llm=self._llm,
+                prompt=PromptTemplate.from_template("{prompt}")
+            )
+
+            completion_chain = (
+                    planner_prompt
+                    | self._llm
+            )
+            planner_chain = (
+                    RunnableParallel(completion=completion_chain, prompt_value=planner_prompt)
+                    | RunnableLambda(lambda x: retry_planner_parser.parse_with_prompt(**x))
+            )
+
+            bq_retriever = BatchQueryRetriever(retriever)
+            retrieving_chain = (
+                    planner_chain
+                    | RunnableParallel(retrieved_docs=bq_retriever.do_batch, planned_queries=RunnablePassthrough())
+            )
+
         else:
-            retrieving_chain = RunnableParallel(retrieved_docs=retriever, hyde_doc=lambda x: None)  # todo: simplify
+            retrieving_chain = RunnableParallel(retrieved_docs=retriever)
 
         context_chain = RunnableParallel(
             context=itemgetter('retrieval_result') | prepare_context,
